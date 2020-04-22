0aafd87da42104603c12c732fde44cd07d41c120
lilianbaron <lilianbaron1995@gmail.com>
2020-04-22 22:10:09 +0800

    update

---
+++ lilian/Bootstrap/js/index12.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    $('.bxslider').bxSlider({
+        mode: 'horizontal',
+        moveSlides: 1,
+        slideMargin: 40,
+        infiniteLoop: true,
+        minSlides: 1,
+        maxSlides: 1,
+        speed: 1200
+    });
+})
--- lilian/Bootstrap/js/main.js
+++ lilian/Bootstrap/js/main.js
@@ -1,24 +1,37 @@
-// /**Sticky Nav*/
+ /**Sticky Nav*/
 
 
-// let navbar = $(".navbar");
+let navbar = $(".navbar");
 
-//  $(window).scroll(function(){
-//    let oTop = $("Section-2").offset().top - window.innerHeight;
-//    if($(window).scrollTop() > oTop){
-//      navbar.addClass("sticky");
-//    }else(
-//      navbar.removeClass("sticky");
-//    )
-// });
+ $(window).scroll(function(){
+   let oTop = $("Section-2").offset().top - window.innerHeight;
+   if($(window).scrollTop() > oTop){
+      navbar.addClass("sticky");
+    }else(
+      navbar.removeClass("sticky")
+    )
+ });
 
 
-//  $(document).on("scroll", function(){
-//     if($(document).scrollTop()) {
-//        $('nav').addClass("sticky");
-//      }else{
-//       $('nav').removeClass("sticky");   
-//       }
-//    });
+  $(document).on("scroll", function(){
+     if($(document).scrollTop()) {
+        $('nav').addClass("sticky");
+      }else{
+     $('nav').removeClass("sticky");   
+       }
+    });
+
+    jQuery(document).ready(function(){
+        $('.counter').counterUp({
+            delay: 10,
+            time: 1000
+        });
+    });
+
+
+
+
+
+    
 
     
